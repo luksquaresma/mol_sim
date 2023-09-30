db5bbb780a38821808fbaca1c0c242573b80d207
Lucas Quaresma <lucas.quaresma1@gmail.com>
2023-09-30 10:55:15 -0300

    04 - domain finished

--- mol_sim/src/main.rs
+++ mol_sim/src/main.rs
@@ -5,11 +5,26 @@
 // Constant dfinitions
 pub const PI:f32 = 3.14159;
 pub const DT:f32 = 1./100.;
-pub const MOLECULE_NUMBER:    u32 = 30;
+pub const MOLECULE_NUMBER:    u32 = 5;
 pub const POSITION_DOMAIN:    [[f32; 2]; 3] = [[ 0.,  1.], [ 0.,  2.], [ 5.,  6.]]; //[[x_min, x_max], [y_min, y_max]...]
 pub const VELOCITY_INIT:      [[f32; 2]; 3] = [[-1.,  1.], [-2.,  2.], [ 2.,  6.]]; //[[x/s_min, x_max], [y/s_min, y_max]...]
 pub const ORIENTATION_INIT:   [[f32; 2]; 2] = [[ 0.,  1.], [ 0.,  1.]]; //[[theta_min, theta_max], [phi_min, phi_max]]
 pub const ANGULAR_VEL_INIT:   [[f32; 2]; 2] = [[ 0.,  1.], [ 0.,  1.]]; //[[theta/s_min, theta/s_max], [phi/s_min, phi/s_max]]
+
+pub trait DataStructure {
+    // Returns a list of keys for all the internal data
+    fn get_keys(&self) -> Vec<String>;
+
+    // Returns the item which match a key
+    fn get_value(&self, k:&String) -> &Vec<Vec<f32>>;
+
+    // Returns the amount of information (len(keys))
+    fn get_info_qty(&self) -> usize;
+
+    // Returns a vector of tuples that contins all 
+    // the keys and values in the form (key, value).
+    fn get_contents(&self) -> Vec<(String, Vec<Vec<f32>>)>;
+}
 
 pub struct Conditions {
     pub mn:      u32,           // number of molecules
@@ -26,13 +41,6 @@ pub struct State {
     pub ave: Vec<Vec<f32>>, // angular velocity (degrees/s) - any real value
 }
 
-pub trait DataStructure {
-    fn get_keys(&self) -> Vec<String>;
-    fn get_info_qty(&self) -> usize;
-    fn get_value(&self, k:String) -> &Vec<Vec<f32>>;
-    fn get_contents(&self) -> Vec<String>;
-}
-
 impl DataStructure for State {
     fn get_keys(&self) -> Vec<String> {
         return vec![
@@ -45,7 +53,7 @@ impl DataStructure for State {
     fn get_info_qty(&self) -> usize {
         return self.get_keys().len()
     }
-    fn get_value(&self, k:String) -> &Vec<Vec<f32>> {
+    fn get_value(&self, k:&String) -> &Vec<Vec<f32>> {
         return match k.as_str() {
             "pos" => &self.pos,
             "vel" => &self.vel,
@@ -53,18 +61,16 @@ impl DataStructure for State {
             "ave" => &self.ave,
             &_ => todo!()
         }
+    }  
+    fn get_contents(&self) -> Vec<(String, Vec<Vec<f32>>)> {
+        return self.get_keys().iter().map(
+            |k| (k.clone(), self.get_value(k).clone())
+        ).collect()
     }
-    fn get_contents(&self) -> Vec<String> {    //Vec<(String, Vec<Vec<f32>>)> {
-        return self.get_keys().into_iter().map(
-            |k| k
-            //*self.get_value(*k)
-        ).collect();
-    }
-    
 }
 
 fn create_molecules(cs:Conditions) -> State {  //mn:u32, pd:[[f32; 2]; 3], vi:[[f32; 2]; 3]
-    let (mn, pd, vi, oi, ai) = (cs.mn, cs.pos_dom, cs.vel_ini, cs.ori_ini, cs.ave_ini);
+    let (mn, pd, vi, _oi, _ai) = (cs.mn, cs.pos_dom, cs.vel_ini, cs.ori_ini, cs.ave_ini);
     let pos:Vec<Vec<f32>> = (0..mn).map(|_|
         vec![
             pd[0][0] + (pd[0][1] - pd[0][0])*(rand::random::<f32>()), // x
@@ -105,70 +111,9 @@ fn main() {
 
     let state = create_molecules(starting_cond);
 
-    println!("{:.2?}", state.get_contents());
-
-    assert!(true, "Test is NOT WORKING");
-    assert!(state.get_contents() == state.get_keys(), "Test is NOT WORKING");
-
-
+    for (k, v) in state.get_contents() {
+        println!("KEY: {}",k);
+        println!("{:.2?}", v)
+    };
+    // assert!(state.get_contents() == state.get_keys(), "Test is NOT WORKING");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //returns [position, orientation, velocity]
-// fn create_molecules() -> Vec<[f32; 3]> {
-//     //[u32; N as usize] {
-//     // [[[i32; 3]; N as usize]; 3] {
-//     let _pid:Vec<u32> = (0..N).map(|i| i).collect::<Vec<u32>>(); //p_id: &[u32]
-//     let pos:Vec<[f32; 3]> = (0..N).map(|_| 
-//         [
-//             DOMAIN_SIZE[0][0] + (DOMAIN_SIZE[0][1] - DOMAIN_SIZE[0][0])*(rand::thread_rng().gen::<f32>()),
-//             DOMAIN_SIZE[1][0] + (DOMAIN_SIZE[1][1] - DOMAIN_SIZE[1][0])*(rand::thread_rng().gen::<f32>()),
-//             DOMAIN_SIZE[2][0] + (DOMAIN_SIZE[2][1] - DOMAIN_SIZE[2][0])*(rand::thread_rng().gen::<f32>()),
-//         ]).collect();
-    
-//     // todo: normalize oringin vector (ori)
-//     let ori:Vec<Vec<f32>> = (0..N).map(|_| 
-//         vec![
-//             (rand::thread_rng().gen::<f32>()),
-//             (rand::thread_rng().gen::<f32>()),
-//             (rand::thread_rng().gen::<f32>()),
-//         ]).collect::<Vec<Vec<f32>>>(); //.map(|x| x.pow(2.)));
-
-    
-//     let vel:Vec<[f32; 3]> = (0..N).map(|_| 
-//         [
-//             VELOCITY_DOMAIN[0] + (VELOCITY_DOMAIN[1] - VELOCITY_DOMAIN[0])*(rand::thread_rng().gen::<f32>()),
-//             VELOCITY_DOMAIN[0] + (VELOCITY_DOMAIN[1] - VELOCITY_DOMAIN[0])*(rand::thread_rng().gen::<f32>()),
-//             VELOCITY_DOMAIN[0] + (VELOCITY_DOMAIN[1] - VELOCITY_DOMAIN[0])*(rand::thread_rng().gen::<f32>()),
-//         ]).collect();
-//     return pos;
-// }
-
-// fn main() {
-//     println!("{:.2?}", create_molecules());
-// }
